fec3531f0afd8e5aeec49b03d06dc5f31d51c1a6
fjshomeplate <38842004+fjshomeplate@users.noreply.github.com>
2020-08-24 14:20:27 -0400

    Add Info about Lakeys

--- src/components/bio.js
+++ src/components/bio.js
@@ -38,8 +38,27 @@ const Bio = () => {
   const { author, social } = data.site.siteMetadata
   return (
     <div>
-      <h1>Go Lakey!</h1>
-
+      <h2>
+      The Lakeland Terrier is a small, lovable terrier.
+      </h2>
+      <h3>
+      Some History...
+      </h3>
+      <p>
+      The Lakeland Terrier, also Known as the Patterton Terrier, was bred to hunt foxes and otters in the Lake District. The look somewhat like <a href="http://wikipedia.com/welsh-terrier">Welsh</a> or the <a href='http://wikipedia.com/airedale'>Airedale Terrier</a>.
+      </p>
+      <h3>
+      Coat
+      </h3>
+      <p>
+      The Lakeland is non-shedding, and has a soft coat as a puppy. However, as it ages, wire will start to come in. The AKC Standards state that the dog must be plucked, however, clippering is fine, but the coat may fade and curl.
+      </p>
+      <h3>
+      Tempermant
+      </h3>
+      <p>
+      The Lakeland Terrier is friendly, bold and agreeable, however, as with any terrier, a bit stubborn. The AKC advises early socilization as well as puppy classes.
+      </p>
     </div>
   )
 }
